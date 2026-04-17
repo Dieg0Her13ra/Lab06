2d96c905870a01ce1a92b6388558a8228d7a1f6d
AusDr4ko! <diego0hh013@gmail.com>
2026-04-17 05:02:30 +0000

    Interest module

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import HeaderModule from "./Components/Header/Header";
 import HeroModule from "./Components/Hero/Hero";
-
+import InterestModule from "./Components/InterestSection/InterestSection";
 export default function Home() {
   return (
     <>
     <HeaderModule />
     <HeroModule/>
+    <InterestModule/>
     </>
   );
 }
